a9c142e2fc8de3f4da789e1583416bf9e3c096bf
Laure-Anne-Reau <laure-anne-margaux.reau@polymtl.ca>
2020-09-29 23:36:36 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -92,7 +92,7 @@ def main() -> None:
     recipes = get_recipes({})
     recipes = get_recipes(recipes)
     print("On affiche une recette au choix...")
-    print_recipe()
+    print_recipe(recipes)
     print_recipe(recipes)
 
 
