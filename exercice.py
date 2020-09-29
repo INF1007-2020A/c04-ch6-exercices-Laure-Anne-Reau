6b26594637cbfc48a1adab1e7698e7e6647a20f2
Laure-Anne-Reau <laure-anne-margaux.reau@polymtl.ca>
2020-09-29 23:01:43 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -5,7 +5,7 @@ import string
 
 #def order(values: list = None) -> list:
 #    if values is None:
-        # TODO: demander les valeurs ici
+#        # TODO: demander les valeurs ici
         Liste = input("veuillez entrer les valeurs séparées par une virgule: ")
         
         pass
