9aee90bce7e179ef91bbd4f92f3ad01b8f2e6195
Laure-Anne-Reau <laure-anne-margaux.reau@polymtl.ca>
2020-09-29 23:34:40 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -67,8 +67,8 @@ def get_recipes(dictionnaire):
 def print_recipe(ingredients) -> None:
     # TODO: Demander le nom d'une recette, puis l'afficher si elle existe
     name = input("Entrez le nom d'une recette.\n")
-    if name in ingrediend
-    ingrediants[name]
+    if name in ingredients:
+        ingrediants[name]
 
 
 def main() -> None:
