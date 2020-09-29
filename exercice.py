f315e32480f81de648926cc0653003339341f4ac
Laure-Anne-Reau <laure-anne-margaux.reau@polymtl.ca>
2020-09-29 23:30:00 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -55,7 +55,7 @@ def frequence(sentence: str) -> dict:
     return {}
 
 
-def get_recipes(dict):
+def get_recipes(dictionnaire):
     # TODO: Demander le nom d'une recette, puis ses ingredients et enregistrer dans une structure de données
     name = input("Entrez le nom de votre recette.\n")
     ingrediants = input("Entrez la liste d'ingrédients de la recette, svp séparer les ingrédients par une virgule.\n")
