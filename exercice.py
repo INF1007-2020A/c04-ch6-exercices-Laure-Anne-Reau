7d1babf42a6641917464e72b30a7f50e336cedf3
Laure-Anne-Reau <laure-anne-margaux.reau@polymtl.ca>
2020-09-29 22:25:23 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -31,7 +31,7 @@ def best_grades(student_grades: dict) -> dict:
         avg = sum(value) / len(value)
         if len(best_student) == 0 or list(best_student.values())[0] < avg:
             best_student = (key: avg)
-    return {}
+    return best
 
 
 def frequence(sentence: str) -> dict:
