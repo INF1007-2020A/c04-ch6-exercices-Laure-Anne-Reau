3c7514acc210601051cd1c7ba1bed2ed6a58d6da
Laure-Anne-Reau <laure-anne-margaux.reau@polymtl.ca>
2020-09-29 22:40:03 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -40,7 +40,8 @@ def best_grades(student_grades: dict) -> dict:
 def frequence(sentence: str) -> dict:
     # TODO: Afficher les lettres les plus fréquentes
     #       Retourner le tableau de lettres
-    sentence = dict
+    sentence = list
+    
     for key, value in sentence.items():
         compteur = 
 
