39aa29b8e8bdd00d0c2a8bc5b7dcdcb00c02d8a2
Laure-Anne-Reau <laure-anne-margaux.reau@polymtl.ca>
2020-09-29 23:38:39 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -71,7 +71,7 @@ def print_recipe(ingredients) -> None:
         print(ingredients)
     else :
         print("La recette n'existe pas!\n")
-        print("Les recettes existentes sont: {ingre}")
+        print("Les recettes existentes sont: {ingredients.keys()}")
         print_recipe(ingredients)
 
 
